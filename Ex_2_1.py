ee030077a3d2458de04632d6848ba26474fbd62b
Grigory Plakhotnikov <69365763+gplakhotnikov@users.noreply.github.com>
2025-04-12 10:35:54 -0300

    Create Ex_2_1.py

---
+++ Ex_2_1.py
@@ -0,0 +1,11 @@
+nomes = ['Maria', 'Julieta', 'Fernando', 'Cristiano', 'Julieta', 'Maria', 'Fernando', 'Cláudio']
+qtd_letras = {}
+for nome in nomes:
+    qtd_letras [nome] = len(nome)
+
+print(qtd_letras)
+
+"""
+Saída: 
+{'Maria': 5, 'Julieta': 7, 'Fernando': 8, 'Cristiano': 9, 'Cláudio': 7}
+"""
